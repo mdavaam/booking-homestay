27143789489415b8285ce0254428e02f2c524356
Izmi Ukhti <izmiukhti@gmail.com>
2025-09-08 22:50:43 +0700

    perbaikan code duplication

--- resources/views/layouts/sidebar-top.blade.php
+++ resources/views/layouts/sidebar-top.blade.php
@@ -17,9 +17,6 @@
         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
         <i class="fas fa-sign-out-alt me-2"></i> Logout
     </a>
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-    </form>
 
     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
         @csrf
